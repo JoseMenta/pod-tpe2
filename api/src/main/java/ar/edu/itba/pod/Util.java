3bf7cc0ed7bd4ab59d280295909a611fde9d02b3
JoseMenta <jrmenta2@gmail.com>
2024-05-30 15:43:55 -0300

    Add server configuration
    Co-authored-by: francoisgaston <gfrancois@itba.edu.ar>
    Co-authored-by: AxelPreitiT <axel.preitit@gmail.com>
    Co-authored-by: Laucha12 <lautihernando@gmail.com>

---
+++ api/src/main/java/ar/edu/itba/pod/Util.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.pod;
+
+public class Util {
+
+    //TODO: use config file
+    public static final String HAZELCAST_GROUP_NAME = "g6";
+    public static final String HAZELCAST_GROUP_PASSWORD = "pod";
+
+
+    public static final String HAZELCAST_NETWORK_MASK = "10.103.1.*";
+}
--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -1,13 +1,43 @@
 package ar.edu.itba.pod.server;
 
+import ar.edu.itba.pod.Util;
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 
 public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        logger.info("Hello Server!");
-    }}
+        LOGGER.info("Starting Hazelcast server");
+        //Config
+        Config config = new Config();
+
+        //Configure group
+        GroupConfig groupConfig = new GroupConfig()
+                .setName(Util.HAZELCAST_GROUP_NAME)
+                .setPassword(Util.HAZELCAST_GROUP_PASSWORD);
+        config.setGroupConfig(groupConfig);
+
+        //Network config
+        MulticastConfig multicastConfig = new MulticastConfig();
+        JoinConfig joinConfig = new JoinConfig().setMulticastConfig(multicastConfig);
+        InterfacesConfig interfacesConfig = new InterfacesConfig()
+                .setInterfaces(Collections.singletonList(Util.HAZELCAST_NETWORK_MASK))
+                .setEnabled(true);
+
+        NetworkConfig networkConfig = new NetworkConfig()
+                .setInterfaces(interfacesConfig)
+                .setJoin(joinConfig);
+
+        config.setNetworkConfig(networkConfig);
+
+        // Start cluster
+        Hazelcast.newHazelcastInstance(config);
+
+    }
+}

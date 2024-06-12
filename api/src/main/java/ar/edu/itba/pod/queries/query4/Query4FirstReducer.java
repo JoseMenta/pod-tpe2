49bf14cfdbd05521805fa41e27ed8b5bc4b6ea58
JoseMenta <jrmenta2@gmail.com>
2024-06-12 20:37:56 -0300

    change query4 to use more reducing reducer

--- api/src/main/java/ar/edu/itba/pod/queries/query4/Query4FirstReducer.java
+++ api/src/main/java/ar/edu/itba/pod/queries/query4/Query4FirstReducer.java
@@ -4,33 +4,44 @@ import ar.edu.itba.pod.data.Pair;
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
-public class Query4FirstReducer implements ReducerFactory<Pair<String, String>, Integer, Integer> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Query4FirstReducer implements ReducerFactory<String, String, Pair<String,Integer>> {
 
     /**
      * Given the (<neighbourhood, plate>, 1) pairs, returns the (<neighbourhood, plate>, total) pair
      * where total is the sum of ones (amount of pairs with same key)
      */
     @Override
-    public Reducer<Integer, Integer> newReducer(Pair<String, String> p) {
+    public Reducer<String, Pair<String,Integer>> newReducer(String key) {
         return new Query4Reducer();
     }
 
-    private class Query4Reducer extends Reducer<Integer, Integer> {
-        private int sum = 0;
+    private class Query4Reducer extends Reducer<String, Pair<String,Integer>> {
+
+        transient Map<String, Integer> aux;
 
         @Override
         public void beginReduce() {
-            this.sum = 0;
+             this.aux = new HashMap<>();
         }
 
         @Override
-        public void reduce(Integer integer) {
-            this.sum += integer;
+        public void reduce(String plate) {
+            aux.putIfAbsent(plate,0);
+            aux.put(plate,aux.get(plate)+1);
         }
 
         @Override
-        public Integer finalizeReduce() {
-            return sum;
+        public Pair<String,Integer> finalizeReduce() {
+            Map.Entry<String, Integer> entry = null;
+            for(Map.Entry<String, Integer> auxEntry : aux.entrySet()) {
+                if(entry == null || entry.getValue() < auxEntry.getValue()){
+                    entry = auxEntry;
+                }
+            }
+            return new Pair<>(entry.getKey(),entry.getValue());
         }
     }
 }
